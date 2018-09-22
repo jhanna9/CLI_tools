389b508eda99fefe09264659e392d39479328630
jhanna9 <hannajm9@gmail.com>
2018-09-21 21:42:33 -0700

    added args

--- remove_files/remove_files.py
+++ remove_files/remove_files.py
@@ -2,10 +2,12 @@
 
     cli flags:
 
+    path to file
     file name (optional)
     file extension (optional)
     files size (optional)
 '''
+import argparse
 import os
 
 my_path = 'C:/Users/J/Documents/python/book1-exercises/chp09/practice_files/little pics'
@@ -33,3 +35,21 @@ path to folder containing files to be deleted
 help section
 
 '''
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help='Path to the files.')
+    parser.add_argument('-nm', '--name', help='The name of the file to be deleted.', action='store_true')
+    parser.add_argument('-ext', '--extension', help='The type of extension to be deleted', action='store_true')
+    parser.add_argument('-s', '--size', help='The minimum size of the file to be deleted', action='store_true')
+    args = parser.parse_args()
+
+    if args.private:
+        print('ig account is private')
+    else:
+        get_content(get_links(args.username))
+
+
+if __name__ == '__main__':
+    main()
